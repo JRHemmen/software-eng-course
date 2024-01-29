791937b3cd431ce3276f8d55711da06abf01085e
James Hemmen <JRHemmen@icloud.com>
2024-01-29 13:12:03 -0500

    add demos

---
+++ godemo/functional.go
@@ -0,0 +1,23 @@
+package godemo
+
+import "fmt"
+
+func FunctionalProgramming() {
+
+	// Pure Functions
+	fmt.Println("Pure Functions:")
+	fmt.Println("Pure functions are functions that do not modify any external state")
+
+	// Immutability
+
+	// Higher Order Functions
+
+	// Recursion
+
+	// Anonymous Functions
+	fmt.Println("Anonymous functions:")
+	func() {
+		fmt.Println("Anonymous functions are useful for inline, one-time use")
+	}()
+
+}
---
+++ godemo/memory.go
@@ -0,0 +1,31 @@
+package godemo
+
+import "fmt"
+
+func Memory() {
+	// Data Types
+	var integer int = 42
+	var floatingPoint float64 = 3.1415
+	var str string = "Hello, Go!"
+	var boolean bool = true
+
+	fmt.Println("Data Types:")
+	fmt.Printf("Integer: %d, Floating Point: %f, String: %s, Boolean: %t\n\n", integer, floatingPoint, str, boolean)
+
+	// Variables and Memory
+	fmt.Println("Variables and Memory:")
+	fmt.Printf("Integer address: %p, Floating Point address: %p\n\n", &integer, &floatingPoint)
+
+	// Stack and Heap
+	fmt.Println("Stack and Heap:")
+	stackVar := "Stored on the stack"
+	heapVar := new(string)
+	*heapVar = "Stored on the heap"
+	fmt.Printf("Stack variable: %s, Heap variable: %s\n\n", stackVar, *heapVar)
+
+	// Pointers and References
+	fmt.Println("Pointers and References:")
+	ptr := &integer
+	fmt.Printf("Pointer to integer: %p, Dereferenced pointer: %d\n\n", ptr, *ptr)
+
+}
